84b8d8c9054e6d2d2879ecd524ec13c7c4d27e89
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-10-18 14:35:41 +0200

    [REF] Allow same module name across different repositories
    This change allows to get a module `x` present in different
    repositories (unicity constraint on `odoo.module.branch` has changed
    accordingly). This is useful in case two projects have `x` installed but
    the code of these modules (that can be different) are hosted in their
    own project repository.
    
    Therefore, the scan of repositories and project modules import have been
    adapted to fullfil this new possibility.
    
    Features:
        - allows multiple instances of `<odoo.module.branch>` records
          sharing the same module technical name and branch but belonging to
          different repositories,
        - repositories have now a new `specific` flag, propagated to their
          modules.. By default they host generic modules, but project repositories
          will host specific modules that cannot be shared to other projects,
        - a generic module cannot depend on a specific module
        - when importing modules in a project, specific modules of other
          projects cannot be mapped,
        - once a specific module is scanned in a project repository, relevant
          orphaned modules installed in a project will be re-mapped to this newly
          created specific module,
        - detect PR only on generic modules (unmerged/pending modules are
          generic only, specific modules have to be found in project
          repositories).

--- odoo_repository/tests/__init__.py
+++ odoo_repository/tests/__init__.py
@@ -3,4 +3,4 @@ from . import test_base_scanner
 from . import test_repository_scanner
 from . import test_odoo_repository_scan
 from . import test_sync_node
-from . import test_module_dependency_level
+from . import test_odoo_module_branch
